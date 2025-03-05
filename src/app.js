58a04673728c95934a69cd542fea9904b0f8c43f
SantosAlexsandro <web_alexsandro@hotmail.com>
2025-03-05 17:49:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -52,8 +52,8 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     // this.app.use(helmet());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+    this.app.use(express.json({ limit: '50mb' }));
     // this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 //
