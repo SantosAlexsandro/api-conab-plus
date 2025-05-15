100afe54f65d8aff282b1f19706815cbd6a654f0
SantosAlexsandro <web_alexsandro@hotmail.com>
2025-05-15 17:28:17 -0300

    Update workShiftRoutes.js

--- src/routes/workShiftRoutes.js
+++ src/routes/workShiftRoutes.js
@@ -3,20 +3,21 @@ import workShiftController from '../controllers/WorkShiftController';
 import { WorkShiftValidator } from '../validators/WorkShiftValidator';
 import authUser from '../middlewares/authUser';
 import validation from '../middlewares/validation';
+import checkPermission from '../middlewares/checkPermission';
 
 const router = new Router();
 
-// Rotas básicas CRUD
-router.get('/', workShiftController.index);
-router.post('/', WorkShiftValidator.validateStore(), validation, workShiftController.store);
-router.get('/:id', WorkShiftValidator.validateId(), validation, workShiftController.show);
-router.put('/:id', WorkShiftValidator.validateUpdate(), validation, workShiftController.update);
-router.delete('/:id', WorkShiftValidator.validateId(), validation, workShiftController.delete);
+// Rotas específicas (devem vir antes das genéricas)
+router.get('/active', authUser, checkPermission('workshift.view'), workShiftController.getActiveShifts);
+router.get('/user/:userCode', authUser, checkPermission('workshift.view'), WorkShiftValidator.validateUserCode(), validation, workShiftController.getUserShifts);
+router.get('/date-range', authUser, checkPermission('workshift.view'), WorkShiftValidator.validateDateRange(), validation, workShiftController.getShiftsByDateRange);
+router.post('/check-overlap', authUser, checkPermission('workshift.view'), WorkShiftValidator.validateOverlapCheck(), validation, workShiftController.checkOverlap);
 
-// Rotas específicas
-router.get('/active', workShiftController.getActiveShifts);
-router.get('/user/:userCode', WorkShiftValidator.validateUserCode(), validation, workShiftController.getUserShifts);
-router.get('/date-range', WorkShiftValidator.validateDateRange(), validation, workShiftController.getShiftsByDateRange);
-router.post('/check-overlap', WorkShiftValidator.validateOverlapCheck(), validation, workShiftController.checkOverlap);
+// Rotas básicas CRUD
+router.get('/', authUser, checkPermission('shifts.view'), workShiftController.index);
+router.post('/', authUser, checkPermission('shifts.create'), WorkShiftValidator.validateStore(), validation, workShiftController.store);
+router.get('/:id', authUser, checkPermission('shifts.view'), WorkShiftValidator.validateId(), validation, workShiftController.show);
+router.put('/:id', authUser, checkPermission('shifts.edit'), WorkShiftValidator.validateUpdate(), validation, workShiftController.update);
+router.delete('/:id', authUser, checkPermission('shifts.delete'), WorkShiftValidator.validateId(), validation, workShiftController.delete);
 
 export default router;
